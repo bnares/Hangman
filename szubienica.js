2b0aced9115a68dde1552e7b2f7580a63093f14d
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-03-21 14:53:45 +0100

    finding letters

--- szubienica.js
+++ szubienica.js
@@ -30,6 +30,44 @@ function wypisz_haslo()
 
 window.onload = start;
 
+var litery = new Array(35);
+
+litery[0] = "A";
+litery[1]="Ą";
+litery[2]="B";
+litery[3]="C";
+litery[4]="Ć";
+litery[5]="D";
+litery[6]="E";
+litery[7]="Ę";
+litery[8]="F";
+litery[9]="G";
+litery[10]="H";
+litery[11]="I";
+litery[12]="J";
+litery[13]="K";
+litery[14]="L";
+litery[15]="Ł";
+litery[16]="M";
+litery[17]="N";
+litery[18]="Ń";
+litery[19]="O";
+litery[20]="Ó";
+litery[21]="P";
+litery[22]="Q";
+litery[23]="R";
+litery[24]="S";
+litery[25]="Ś";
+litery[26]="T";
+litery[27]="U";
+litery[28]="V";
+litery[29]="W";
+litery[30]="X";
+litery[31]="Y";
+litery[32]="Z";
+litery[33]="Ż";
+litery[34]="Ż";	
+
 function start()
 {
 	
@@ -37,8 +75,8 @@ function start()
 	
 	for(i = 0; i<35; i++)
 	{
-	
-		tresc_diva = tresc_diva+'<div class = "litera">A</div>';
+		var element = 'lit'+i;
+		tresc_diva = tresc_diva+'<div class = "litera" onclick ="sprawdz('+i+')"   id = "'+element+'">'+litery[i]+'</div>';
 		if((i+1)%7==0) 
 		{
 		tresc_diva = tresc_diva+'<div style = "clear:both;"></div>';	
@@ -51,4 +89,45 @@ function start()
 	
 	
 	wypisz_haslo();
+}
+
+
+String.prototype.ustawZnak = function(miejsce, znak)
+{
+
+	if(miejsce>this.length-1) return this.toString();
+	else return this.substr(0,miejsce)+znak+this.substr(miejsce+1);
+	
+}
+
+
+function sprawdz(numer)
+{
+	
+	var trafiona = false;
+	for(i = 0; i<haslo1; i++)
+	{
+		
+		if(haslo.charAt(i)==litery[numer])
+		{
+			haslo2 = haslo2.ustawZnak(i, litery[numer]);
+			trafiona = true;
+			
+		}
+		
+	}
+	
+	if(trafiona==true)
+	{
+		var element = 'lit'+numer;
+		document.getElementById(element).style.background = "#003300";
+		wypisz_haslo();
+	}
+	else
+	{
+		
+	}
+	
+	
+	
 }
