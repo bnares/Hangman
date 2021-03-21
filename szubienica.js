849c2a56999ee19f83730fe07b762259095dc596
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-03-21 13:18:16 +0100

    dashboard

--- szubienica.js
+++ szubienica.js
@@ -1,12 +1,54 @@
 
 
-var haslo = "bez pracy nie ma kolaczy";
+var haslo = "bez pracy nie ma kołaczy";
+haslo = haslo.toUpperCase();
+
+var haslo1 = haslo.length;
+var haslo2="";
+
+for(i =0; i<haslo1; i++)
+{
+	if(haslo.charAt(i)!=" ")
+	{
+		
+		haslo2 = haslo2+"-";
+	
+	}
+	else
+	{
+			haslo2 = haslo2+" ";
+	}
+	
+}
 
 function wypisz_haslo()
 {
 	
-	document.getElementById("plansza").innerHTML = haslo;
+	document.getElementById("plansza").innerHTML = haslo2;
 	
 }
 
-window.onload = wypisz_haslo;
+window.onload = start;
+
+function start()
+{
+	
+	var tresc_diva = "";
+	
+	for(i = 0; i<35; i++)
+	{
+	
+		tresc_diva = tresc_diva+'<div class = "litera">A</div>';
+		if((i+1)%7==0) 
+		{
+		tresc_diva = tresc_diva+'<div style = "clear:both;"></div>';	
+		}
+	
+	}
+	
+	document.getElementById("alfabet").innerHTML = tresc_diva;
+	
+	
+	
+	wypisz_haslo();
+}
